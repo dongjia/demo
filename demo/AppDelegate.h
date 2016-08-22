a5793f493f1d6676d2487a7f6312624c7d637165
dongjia <apple@appletekiMacBook-Pro-3.local>
2016-08-22 10:51:17 +0800

    first commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by dongjia_pc on 16/8/22.
+//  Copyright © 2016年 dongjia_pc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by dongjia_pc on 16/8/22.
+//  Copyright © 2016年 dongjia_pc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
